b7523f680038335c6a4ac81f5c7e51873241327f
Marcio Bueno <marciobueno@gmail.com>
2024-08-16 23:02:23 +0000

    Primeiro site com JavaScript

---
+++ ex01/script.js
@@ -0,0 +1,8 @@
+// alert("Olá!");
+console.log("Mensagem impressa no console das ferramentas do desenvolvedor!");
+
+// console.log(alert("esse aqui é um alert!"));
+// console.log(confirm("esse aqui é um confirm"));
+// console.log(prompt("esse aqui é um prompt"));
+
+document.getElementById("titulo").innerHTML = "Alterando o título 1";
