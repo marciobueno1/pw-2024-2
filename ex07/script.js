ba13483cfa35dc11912b4d147f3e4b22b9d0848c
Marcio Bueno <marciobueno@gmail.com>
2024-10-01 19:36:25 -0300

    ex07 - finalizado CRUD com back4app

--- ex07/script.js
+++ ex07/script.js
@@ -1,4 +1,16 @@
 const olTarefas = document.getElementById("olTarefas");
+const inputDescricao = document.getElementById("inputDescricao");
+const btAdicionar = document.getElementById("btAdicionar");
+
+const baseURL = "https://parseapi.back4app.com/classes/Tarefa";
+const headers = {
+  "X-Parse-Application-Id": "4cRNaPFAu9tGO59OrBIJpH4v6qeHUUbtReuVmjP7",
+  "X-Parse-REST-API-Key": "x3OZm4ZBkTNLy3jJUc9JImiuAi6XK4ih7zfKOVwk",
+};
+const headersJson = {
+  ...headers,
+  "Content-Type": "application/json",
+};
 
 const createList = (data) => {
   olTarefas.innerHTML = "";
@@ -10,24 +22,62 @@ const createList = (data) => {
     const cb = document.createElement("input");
     cb.type = "checkbox";
     cb.checked = tarefa.concluida;
-    cb.disabled = true;
+    cb.onchange = () => handleCheckboxClick(cb, tarefa);
     li.appendChild(cb);
+    const bt = document.createElement("button");
+    bt.innerHTML = "x";
+    bt.onclick = () => handleBtRemoverClick(bt, tarefa);
+    li.appendChild(bt);
     olTarefas.appendChild(li);
   });
 };
 
+const handleCheckboxClick = async (cb, tarefa) => {
+  try {
+    cb.disabled = true;
+    const response = await fetch(`${baseURL}/${tarefa.objectId}`, {
+      method: "PUT",
+      headers: headersJson,
+      body: JSON.stringify({ concluida: !tarefa.concluida }),
+    });
+    cb.disabled = false;
+    console.log(response);
+    if (!response.ok) {
+      cb.checked = !cb.checked;
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+  } catch (error) {
+    cb.checked = !cb.checked;
+    console.log(error);
+  }
+};
+
+const handleBtRemoverClick = async (bt, tarefa) => {
+  try {
+    bt.disabled = true;
+    const response = await fetch(`${baseURL}/${tarefa.objectId}`, {
+      method: "DELETE",
+      headers: headers,
+    });
+    bt.disabled = false;
+    console.log(response);
+    if (!response.ok) {
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+    getTarefas();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const getTarefas = async () => {
   try {
-    const response = await fetch(
-      "https://parseapi.back4app.com/classes/Tarefa",
-      {
-        method: "GET",
-        headers: {
-          "X-Parse-Application-Id": "4cRNaPFAu9tGO59OrBIJpH4v6qeHUUbtReuVmjP7",
-          "X-Parse-REST-API-Key": "x3OZm4ZBkTNLy3jJUc9JImiuAi6XK4ih7zfKOVwk",
-        },
-      }
-    );
+    const response = await fetch(baseURL, {
+      method: "GET",
+      headers: headers,
+    });
     console.log(response);
     if (!response.ok) {
       alert("Erro ao acessar o servidor: " + response.status);
@@ -40,4 +90,31 @@ const getTarefas = async () => {
   }
 };
 
+const handleBtAdicionarClick = async () => {
+  const descricao = inputDescricao.value.trim();
+  if (!descricao) {
+    alert("Necessário adicionar uma descrição para criar a tarefa!");
+    inputDescricao.focus();
+    return;
+  }
+  try {
+    const response = await fetch(baseURL, {
+      method: "POST",
+      headers: headersJson,
+      body: JSON.stringify({ descricao: descricao }),
+    });
+    console.log(response);
+    if (!response.ok) {
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+    inputDescricao.value = "";
+    inputDescricao.focus();
+    getTarefas();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 window.onload = getTarefas;
+btAdicionar.onclick = handleBtAdicionarClick;
